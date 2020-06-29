145b2f91ee78cbea9105a74aaafc333345e601d8
DannyLuo1993 <33973575+DannyLuo1993@users.noreply.github.com>
2020-06-29 19:29:57 +0800

    day8_20200629
    完成了13课的练习。

--- score_record/app/src/main/java/com/danny/score_record/ScoreViewModel.java
+++ score_record/app/src/main/java/com/danny/score_record/ScoreViewModel.java
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModel;
 public class ScoreViewModel extends ViewModel {
     private MutableLiveData<Integer> teama_score;
     private MutableLiveData<Integer> teamb_score;
+    int teamA_minus_score = 0;
+    int teamB_minus_score = 0;
 
     public MutableLiveData<Integer> getScoreOfTeamA(){
         if(teama_score == null){
@@ -24,35 +26,35 @@ public class ScoreViewModel extends ViewModel {
     };
 
     public void addScoreToTeamA(int n){
-        if(teama_score.getValue() != null){
-        switch (n){
-            case 1:
-                teama_score.setValue(teama_score.getValue() + 1);
-                break;
-            case 2:
-                teama_score.setValue(teama_score.getValue() + 2);
-                break;
-            case 3:
-                teama_score.setValue(teama_score.getValue() + 3);
-                break;
-        }
-
+    if(teama_score.getValue() != null){
+        teama_score.setValue(teama_score.getValue() + n);
+        teamA_minus_score =n;
+        teamB_minus_score =0;
     }}
 
     public void addScoreToTeamB(int n){
-        if(teamb_score.getValue() != null){
-            switch (n){
-                case 1:
-                    teama_score.setValue(teamb_score.getValue() + 1);
-                    break;
-                case 2:
-                    teama_score.setValue(teamb_score.getValue() + 2);
-                    break;
-                case 3:
-                    teama_score.setValue(teamb_score.getValue() + 3);
-                    break;
-            }
-
+     if(teamb_score.getValue() != null){
+            teamb_score.setValue(teamb_score.getValue() + n);
+            teamB_minus_score =n;
+            teamA_minus_score =0;
         }}
 
+    public void minusScore(){
+        while(teamA_minus_score == 0 && teamB_minus_score != 0){
+            teamb_score.setValue(teamb_score.getValue() - teamB_minus_score);
+            teamB_minus_score = 0;
+            break;
+        }
+        while(teamB_minus_score == 0 & teamA_minus_score != 0){
+            teama_score.setValue(teama_score.getValue() - teamA_minus_score);
+            teamA_minus_score = 0;
+            break;
+        }}
+
+    public void refreshScore(){
+        teamA_minus_score = 0;
+        teamB_minus_score = 0;
+        teama_score.setValue(0);
+        teamb_score.setValue(0);
+    }
 }
