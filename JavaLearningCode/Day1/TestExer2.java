aef4ec98d4430277f4ef02509ecddabb25e8fbc9
DannyLuo <dannyluo1993@gmail.com>
2020-08-09 00:33:44 +0800

    day49_20200808

---
+++ JavaLearningCode/Day1/TestExer2.java
@@ -0,0 +1,11 @@
+class TestExer2{
+    public static void main (String[] args){
+        String name = "Danny";
+        int age = 27;
+        boolean ismarry = false;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println("marry"+ismarry);
+    }
+}
---
+++ JavaLearningCode/Day1/TestExer3.java
@@ -0,0 +1,16 @@
+class TestExer3{
+    public static void main(String[] args){
+        String bookName = "《从入门到放弃》";
+        String author = "Danny";
+        double price = 100.0;
+        int sale = 0;
+        int amount = 100;
+        boolean discount = true;
+
+        System.out.println("书名： " + bookName);
+        System.out.println("作者： " + author);
+        System.out.println("价格： " + price);
+        System.out.println("折扣： " + discount);
+
+    }
+}
